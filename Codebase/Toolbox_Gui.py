00eb165be145a4b647612a3f3ff84f0aa4329d65
aMazeeh <82601596+aMazeeh@users.noreply.github.com>
2021-12-14 22:24:18 +0100

    Deleted unnecessary code and implemented adjustable windowsize

--- Codebase/Toolbox_Gui.py
+++ Codebase/Toolbox_Gui.py
@@ -3,9 +3,8 @@ from tkinter import *
 # initiate window
 win = Tk()
 win.title('Project Toolbox')
-win_width=400
-win_height=400
-win.geometry(f"{win_width}x{win_height}")
+win_width=0
+win_height=0
 
 # creating frames
 start_screen_frame = Frame(win, bg='grey')
@@ -18,23 +17,35 @@ settings_screen_frame = Frame(win, bg='grey')
 # pack frames
 def start_screen():
     hide_all_frames()
+
+    # resize the window for the current tool
+    win_height=400 # 400x400 is base size
+    win_width=400
+    win.geometry(f"{win_width}x{win_height}")
+
+    #pack the frame and place buttons
     start_screen_frame.pack(fill="both", expand=1)
 
     bubbleView_button = Button(start_screen_frame, text='Bubble View', command=bubbleView_screen)
     bubbleView_button.place(relx=0.15, rely=0.15, relheight=0.1, relwidth=0.7)
 
-    codeCharts_button = Button(start_screen_frame, text='Code Charts', width=40, command=codeCharts_screen)
+    codeCharts_button = Button(start_screen_frame, text='Code Charts', command=codeCharts_screen)
     codeCharts_button.place(relx=0.15, rely=0.35, relheight=0.1, relwidth=0.7)
 
-    datenanalyse_button = Button(start_screen_frame, text='Datenanalyse', width=40, command=datenanalyse_screen)
+    datenanalyse_button = Button(start_screen_frame, text='Datenanalyse', command=datenanalyse_screen)
     datenanalyse_button.place(relx=0.15, rely=0.55, relheight=0.1, relwidth=0.7)
 
-    settings_button = Button(start_screen_frame, text='Einstellungen', width=40, command=settings_screen)
+    settings_button = Button(start_screen_frame, text='Einstellungen', command=settings_screen)
     settings_button.place(relx=0.15, rely=0.75, relheight=0.1, relwidth=0.7)
 
 
 def bubbleView_screen():
     hide_all_frames()
+
+    # resize the window for the current tool
+    win_height=700 # numbers just for testing purpose
+    win_width=900
+    win.geometry(f"{win_width}x{win_height}")
 
     # pack the frame and place Button and Canvas
     bubbleView_screen_frame.pack(fill="both", expand=1)
@@ -44,10 +55,6 @@ def bubbleView_screen():
     bubbleView_canvas = Canvas(bubbleView_screen_frame)
     bubbleView_canvas.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.85)
 
-    # resize the window for the picture ?
-    win_height=700
-    win_width=900
-    win.geometry(f"{win_width}x{win_height}")
 
     # just a gapfiller
     bubbleView_label = Label(bubbleView_screen_frame, text='*le Bubble View...')
@@ -57,10 +64,15 @@ def bubbleView_screen():
 def codeCharts_screen():
     hide_all_frames()
 
+    # resize the window for the current tool
+    win_height=700 # numbers just for testing purpose
+    win_width=900
+    win.geometry(f"{win_width}x{win_height}")
+
     # pack the frame and place Button
     codeCharts_screen_frame.pack(fill="both", expand=1)
     codeCharts_back_button = Button(codeCharts_screen_frame, text='Zurück', command=start_screen)
-    codeCharts_back_button.place(relx=0.05, rely=0.85, relheight=0.1, relwidth=0.2)
+    codeCharts_back_button.place(relx=0.05, rely=0.925, relheight=0.05, relwidth=0.2)
 
     # just a gapfiller
     codeCharts_label = Label(codeCharts_screen_frame, text='*le Code Charts...', bg='grey')
@@ -70,10 +82,15 @@ def codeCharts_screen():
 def datenanalyse_screen():
     hide_all_frames()
 
+    # resize the window for the current tool
+    win_height=700 # numbers just for testing purpose
+    win_width=900
+    win.geometry(f"{win_width}x{win_height}")
+
     # pack the frame and place Button
     datenanalyse_screen_frame.pack(fill="both", expand=1)
     datenanalyse_back_button = Button(datenanalyse_screen_frame, text='Zurück', command=start_screen)
-    datenanalyse_back_button.place(relx=0.05, rely=0.85, relheight=0.1, relwidth=0.2)
+    datenanalyse_back_button.place(relx=0.05, rely=0.925, relheight=0.05, relwidth=0.2)
 
     # just a gapfiller
     datenanalyse_label = Label(datenanalyse_screen_frame, text='*le Datenanalyse...', bg='grey')
@@ -82,10 +99,15 @@ def datenanalyse_screen():
 def settings_screen():
     hide_all_frames()
 
+    # resize the window for the current tool
+    win_height=700 # numbers just for testing purpose
+    win_width=900
+    win.geometry(f"{win_width}x{win_height}")
+
     # pack the frame and place Button
     settings_screen_frame.pack(fill="both", expand=1)
     settings_back_button = Button(settings_screen_frame, text='Zurück', command=start_screen)
-    settings_back_button.place(relx=0.05, rely=0.85, relheight=0.1, relwidth=0.2)
+    settings_back_button.place(relx=0.05, rely=0.925, relheight=0.05, relwidth=0.2)
 
     # just a gapfiller
     settings_label = Label(settings_screen_frame, text='*le Einstellungen...', bg='grey')
